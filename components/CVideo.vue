22ae4456301a68c4663ca130b911c6fc7acb0cb9
Akure <75520842+AlexWaisley@users.noreply.github.com>
2022-05-12 19:15:33 +0300

    panelControl_

--- components/CVideo.vue
+++ components/CVideo.vue
@@ -1,27 +1,85 @@
 <template>
-  <div class="root">
-    <video :src="src"></video>
-    <div>
-      <div class="btn">
-          123
+
+
+  <div class="video">
+    <div class="container">
+      <video :src="src"></video>
+    </div>
+
+    <div class="controls">
+      <div class="bottom_controls">
+        <div class="_progress">
+
+        </div>
+        <div class="_retractable">
+
+        </div>
       </div>
+
     </div>
   </div>
+
+
 </template>
 
 
 <style scoped lang="scss">
-.root {
-  background: #0576a2;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 
-  video {
+@mixin flexbox-layout {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.video {
+  background: #31a205;
+  @include flexbox-layout;
+  width: 100%;
+  position: relative;
+  height: 100%;
+  .container {
+    @include flexbox-layout;
     width: 100%;
     height: 100%;
+    z-index: 0;
+    video{
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .controls {
+
+    z-index: 1;
+    @include flexbox-layout;
+    background: green;
+    width: 100%;
+    height: 100%;
+
+    .bottom_controls {
+      position: absolute;
+      transition: height 0.5s;
+      bottom: 0;
+      width: 100%;
+      height: 5%;
+      pointer-events: none;
+      @include flexbox-layout;
+      justify-content: flex-start;
+      ._progress {
+        width: 100%;
+        flex-basis: 20%;
+        background: #ff0000;
+      }
+      ._retractable{
+        width: 100%;
+        flex-basis: 80%;
+        pointer-events: all;
+      }
+
+    }
+    .bottom_controls:hover {
+      height: 10%;
+    }
   }
 }
 
