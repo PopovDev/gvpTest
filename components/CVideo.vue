4a7521c7c20f1275b10ceb49513cb33d63903a4c
Andrey <andreypopov2628007@gmail.com>
2022-05-12 19:29:55 +0300

    dAdd

--- components/CVideo.vue
+++ components/CVideo.vue
@@ -1,13 +1,10 @@
 <template>
-
-
   <div class="video">
     <div class="container">
-      <video :src="src"></video>
+      <video ref="video"></video>
     </div>
-
     <div class="controls">
-      <div class="bottom_controls">
+      <div class="bottom">
         <div class="_progress">
 
         </div>
@@ -38,47 +35,46 @@
   width: 100%;
   position: relative;
   height: 100%;
-  .container {
+  > .container {
     @include flexbox-layout;
     width: 100%;
     height: 100%;
     z-index: 0;
-    video{
+
+    > video {
       width: 100%;
       height: 100%;
     }
   }
-  .controls {
 
+  > .controls {
     z-index: 1;
     @include flexbox-layout;
     background: green;
     width: 100%;
     height: 100%;
 
-    .bottom_controls {
+    > .bottom {
       position: absolute;
       transition: height 0.5s;
       bottom: 0;
       width: 100%;
       height: 5%;
-      pointer-events: none;
       @include flexbox-layout;
       justify-content: flex-start;
-      ._progress {
+
+      > ._progress {
         width: 100%;
         flex-basis: 20%;
         background: #ff0000;
       }
-      ._retractable{
+      > ._retractable {
         width: 100%;
         flex-basis: 80%;
-        pointer-events: all;
       }
-
-    }
-    .bottom_controls:hover {
-      height: 10%;
+      &:hover {
+        height: 10%;
+      }
     }
   }
 }
@@ -88,16 +84,44 @@
 <script lang="ts">
 import Vue from 'vue'
 
+declare interface LoadableVideo {
+  src: string;
+  poster: string;
+  name: string;
+}
+
+declare interface VideoData {
+  videoElement: HTMLVideoElement | null;
+  video: LoadableVideo | null;
+}
+
 export default Vue.extend({
   name: 'CVideo',
-  data() {
+  data(): VideoData {
     return {
-      src: '',
+      videoElement: null,
+      video: null
     }
   },
-  methods: {},
+  methods: {
+    loadVideo(video: LoadableVideo): void {
+      if (this.videoElement) {
+        this.video = video;
+        this.videoElement.src = video.src;
+        this.videoElement.poster = video.poster;
+        this.videoElement.load();
+      }
+    }
+  },
   mounted() {
-    this.src = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4";
+    this.videoElement = this.$refs.video as HTMLVideoElement;
+    const video: LoadableVideo = {
+      src: 'videos/1.mp4',
+      poster: 'videos/1.jpg',
+      name: 'Video 1'
+    }
+    this.loadVideo(video);
+
   }
 
 });
