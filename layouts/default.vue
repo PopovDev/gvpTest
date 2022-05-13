947237a87c3e7b2262eaeb58c67c3988f631bd6e
Akure <75520842+AlexWaisley@users.noreply.github.com>
2022-05-13 14:52:44 +0300

    Up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,40 @@
 <template>
   <div class="app">
     <div class="header">
-        <div>
-            <h1>Aboba</h1>
-        </div>
+      <div>
+        <h1>むだ</h1>
+      </div>
     </div>
-    <nuxt/>
+    <div class="body">
+
+      <nuxt/>
+    </div>
   </div>
 </template>
 
-<style>
-*{
+<style lang="scss">
+* {
   margin: 0;
   padding: 0;
 }
-.app{
+
+.app {
   height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
+  background-color: #414141;
+  > .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #544f85;
+  }
+  > .body {
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+  }
 }
-.header{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #3aa293;
-}
+
 </style>
